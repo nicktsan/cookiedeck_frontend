3000e7c2a0a590fa783bb838f09b30a00dd7f77c
nicktsan <nicktsang.manhay@gmail.com>
2024-04-29 19:51:50 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'cookiedeck_frontend';
+  handleToggleChange() {
+    console.log("toggle change test")
+  }
 }
